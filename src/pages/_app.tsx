206c5e1c23a5c14d50775588e05a99d7ccf9d0db
Girish <Girish@sparkbrains.com>
2023-05-24 10:52:17 +0530

    wokred on jira issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,8 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
     <Head>
-      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" /> */}
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#F5F5F5" />
       <title>Electric Bikes</title>
