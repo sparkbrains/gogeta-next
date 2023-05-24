18508433b43090fc04c710025c3f6d887de1d4d5
Girish <Girish@sparkbrains.com>
2023-05-24 10:45:16 +0530

    wokred on jira issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import '../style/main.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { Suspense } from 'react'
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
     <Head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" /> */}
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#F5F5F5" />
       <title>Electric Bikes</title>
