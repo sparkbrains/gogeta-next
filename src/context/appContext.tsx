7e9c5da4d2af661284f9175f31de74f603353b0b
Girish <Girish@sparkbrains.com>
2023-05-31 22:40:31 +0530

    worked on b2b marketplace plp page

--- src/context/appContext.tsx
+++ src/context/appContext.tsx
@@ -1,26 +1,28 @@
 import React, { Component, createContext } from 'react'
-import {withRouter} from 'next/router';
+import { withRouter } from 'next/router';
 export const MainContext = createContext({});
 interface IProps {
     children: any;
 }
 class AppContext extends Component<any> {
-    constructor(props:any){
+    constructor(props: any) {
         super(props)
-        this.state= {
-            host:process.env.NEXT_PUBLIC_APP_ENV
+        this.state = {
+            host: process.env.NEXT_PUBLIC_APP_ENV
         }
     }
-    
+
     componentDidMount(): void {
-        this.setState({
-            ...this.state,
-            host:window.location.host === 'collider.gogeta.bike' ? 'ukMarket':process.env.NEXT_PUBLIC_APP_ENV
-        })
+        if (typeof window !== "undefined") {
+            this.setState({
+                ...this.state,
+                host: window.location.host === 'collider.gogeta.bike' ? 'ukMarket' : process.env.NEXT_PUBLIC_APP_ENV
+            })
+        }
     }
-    
+
     render() {
-    console.log(this.state,window.location.host,window.location.host === 'collider.gogeta.bike','state===');
+        console.log(this.state, 'state===');
         return (
             <MainContext.Provider
                 value={{
@@ -33,7 +35,7 @@ class AppContext extends Component<any> {
     }
 }
 export default withRouter(AppContext);
-export const withContext = (Components:any) => (props:any) => {
+export const withContext = (Components: any) => (props: any) => {
     return (
         <MainContext.Consumer>
             {(value) => <Components context={value} {...props} />}
