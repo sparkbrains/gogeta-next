544faf040896ea644cea39ca45e557cb83cb9eab
Girish <Girish@sparkbrains.com>
2023-06-22 13:03:52 +0530

    change price value

--- src/component/filter/filter_newdesign.tsx
+++ src/component/filter/filter_newdesign.tsx
@@ -382,11 +382,11 @@ export default function Filter({ param, host, filterRes, applyFilterSet, newDesi
 
 
 
-                                                        <div className="d-flex align-item-center justify-content-between pt-2"><div>{profile.currencySymbol + minMaxPrice[0]}</div><div>{profile.currencySymbol + minMaxPrice[1]}</div></div>
-                                                        <div className="d-flex align-item-center justify-content-between pt-3">
+                                                        <div className="d-flex align-item-center justify-content-between pt-2"><div>{profile.currencySymbol + (stateParam[item.inputname]?.length ? stateParam[item.inputname][0] : minMaxPrice[0])}</div><div>{profile.currencySymbol + (stateParam[item.inputname]?.length ? stateParam[item.inputname][1] : minMaxPrice[1])}</div></div>
+                                                        {/* <div className="d-flex align-item-center justify-content-between pt-3">
                                                             <div className="d-flex align-item-center"><label>Min</label>  <div className="slider-range-input">{profile.currencySymbol + (stateParam[item.inputname]?.length ? stateParam[item.inputname][0] : minMaxPrice[0])}</div></div>
                                                             <div className="d-flex align-item-center"><label>Max</label> <div className="slider-range-input">{profile.currencySymbol + (stateParam[item.inputname]?.length ? stateParam[item.inputname][1] : minMaxPrice[1])}</div></div>
-                                                        </div>
+                                                        </div> */}
                                                     </div>
                                                     :
                                                     item.type === 'select' ?
